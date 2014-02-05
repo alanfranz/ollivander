dd653d3d73d9b62797d54735c46d90ab830b1ecd
Jenkins <jenkins@franzoni.eu>
2014-02-05 11:06:49 +0100

    Site updated at 2014-02-05 10:06:49 UTC

--- downloads/code/eu.franzoni.examples/src/multiple_encodings_output.sh
+++
@@ -1,4 +0,0 @@
-echo "utf8 compiled:"
-javac -encoding utf8 eu/franzoni/examples/pythonvsjavaencoding/Encoding.java && java eu.franzoni.examples.pythonvsjavaencoding.Encoding
-echo "windows-1252 compiled:"
-javac -encoding windows-1252 eu/franzoni/examples/pythonvsjavaencoding/Encoding.java && java eu.franzoni.examples.pythonvsjavaencoding.Encoding
